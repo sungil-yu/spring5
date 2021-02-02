580299e7688a427094753fcec484fb787696bc4c
chinggin <chinggin@naver.com>
2021-02-02 18:42:07 +0900

    단순 매핑을 대신할 설정

---
+++ sp5-sp5-chap02/src/main/java/chap14/MvcConfig.java
@@ -0,0 +1,22 @@
+package chap14;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc // <mvc:annotation-driven/>과 같은역할인데 어노테이션으로 등록된빈을 찾아 등록해준다.
+public class MvcConfig implements WebMvcConfigurer{
+
+	//단순 연결을 위해 컨트롤러를 만들어 요청과 맵핑하는 일은 여간 귀찮은 일이 아니다. 
+	//이런 성가심을 없애기위한 매서드이다. 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+			
+		registry.addViewController("/main").setViewName("main");
+		//이 설정은 /main으로오는 요청경로를  view이름이 main인 view를 사용한다고 설정한다.
+	}
+	
+}
